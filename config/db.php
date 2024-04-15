9b5a065c61ca640290b502114547cbb39942b815
Rusu Bogdan <bogdanrusu98@yahoo.ro>
2024-04-15 13:46:05 +0300

    Final Result

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'workopia',
+    'username' => 'sample',
+    'password' => '123456'
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bradtraversy/workopia-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a2418959520f6e15bb630eb65b8e003484bf9dc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bradtraversy/workopia-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a2418959520f6e15bb630eb65b8e003484bf9dc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
